8fe7423e03fba852115b7e394b3a9e22328b7a2b
khairul azmi <khairulazmi@eticketing.my>
2021-04-15 14:16:14 +0800

    Asp.NET Core Web API Versioning

--- my-books/Startup.cs
+++ my-books/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -39,6 +40,18 @@ namespace my_books {
             services.AddTransient<BooksService>();
             services.AddTransient<AuthorsService>();
             services.AddTransient<PublishersService>();
+
+            services.AddApiVersioning(config => {
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                config.AssumeDefaultVersionWhenUnspecified = true;
+
+                //HTTP Header-Based Versioning
+                //config.ApiVersionReader = new HeaderApiVersionReader("custom-version-header");
+
+                //HTTP Media Type-Based Versioning
+                //Content-Type = text/plain;v=1.9
+                //config.ApiVersionReader = new MediaTypeApiVersionReader();
+            });
 
 
             services.AddSwaggerGen(c => {
